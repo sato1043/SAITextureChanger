366e7879414dc1e2cb16840db1ee8412808027db
Toshimitsu Sato <sato1043@gmail.com>
2013-07-11 19:35:02 +0900

    BrowseFolderDialog.csをrevice

--- TextureChanger/util/BrowseFolderDialog.cs
+++ TextureChanger/util/BrowseFolderDialog.cs
@@ -7,215 +7,271 @@ using System.Security.Permissions;
 
 namespace TextureChanger.util
 {
-    public class BrowseFolderDialog : Component
-    {
+	public class BrowseFolderDialog : Component
+	{
+
+		#region 表示時ダイアログメッセージ
+
+		private string _dialogMessage;
+
+		public string DialogMessage
+		{
+			get
+			{
+				return _dialogMessage;
+			}
+			set
+			{
+				_dialogMessage = value;
+			}
+		}
+		#endregion
+
+		#region 結果、取得できたディレクトリパス
+
+		private string _directoryPath;
+
+		public string DirectoryPath
+		{
+			get
+			{
+				return _directoryPath;
+			}
+		}
+		#endregion
+
+		#region 表示時初期ルートノード
+
 		//special folderを示すDWORD定数
-        public enum FolderID
-        {
-            Desktop                 = 0x0000,
-            Printers                = 0x0004,
-            MyDocuments             = 0x0005,
-            Favorites               = 0x0006,
-            Recent                  = 0x0008,
-            SendTo                  = 0x0009,
-            StartMenu               = 0x000b,
-            MyComputer              = 0x0011,
-            NetworkNeighborhood     = 0x0012,
-            Templates               = 0x0015,
-            MyPictures              = 0x0027,
-            NetAndDialUpConnections = 0x0031,
-        }
+		public enum FolderId
+		{
+			Desktop = 0x0000,
+			Printers = 0x0004,
+			MyDocuments = 0x0005,
+			Favorites = 0x0006,
+			Recent = 0x0008,
+			SendTo = 0x0009,
+			StartMenu = 0x000b,
+			MyComputer = 0x0011,
+			NetworkNeighborhood = 0x0012,
+			Templates = 0x0015,
+			MyPictures = 0x0027,
+			NetAndDialUpConnections = 0x0031,
+		};
 
-        private FolderID _startLocation = FolderID.Desktop; //ルートノード初期値
+		private FolderId _startLocation;
 
-        private int _publicOptions =
-			(int)Win32Api.Shell32.BffStyles.RestrictToFilesystem |
-            (int)Win32Api.Shell32.BffStyles.RestrictToDomain ;
+		public FolderId StartLocation
+		{
+			get
+			{
+				return _startLocation;
+			}
+			set
+			{
+				new UIPermission(UIPermissionWindow.AllWindows).Demand();
+				_startLocation = value;
+			}
+		}
 
-        private int _privateOptions = 
-			(int)Win32Api.Shell32.BffStyles.NewDialogStyle ;
+		#endregion
 
-		private string _descriptionText = "フォルダを選択してください：";
+		#region 表示時DWORD値ダイアログオプション（ファイル下部に個別設定用プロパティあり）
 
-        private string directoryPath = String.Empty;
+		private int _publicOptions =
+			(int) Win32Api.Shell32.BffStyles.RestrictToFilesystem |
+			(int) Win32Api.Shell32.BffStyles.RestrictToDomain     ;
 
+		private int _privateOptions =
+			(int)Win32Api.Shell32.BffStyles.NewDialogStyle; //表示時に_publicOptionsに加算されます。
 
+		private void SetOptionField( int mask, bool turnOn )
+		{
+			if (turnOn)
+				_publicOptions |= mask;
+			else
+				_publicOptions &= ~mask;
+		}
+		#endregion
 
-        /// <summary>
-        /// Shows the folder browser dialog box.
-        /// </summary>
-        public DialogResult ShowDialog()
-        {
-            return ShowDialog(null);
-        }
-        
+		// TODO: BFFCALLBACKの実装
 
+		public BrowseFolderDialog( )
+		{
+			_dialogMessage = "フォルダを選択してください：";
+			_directoryPath = String.Empty;
+			_startLocation = FolderId.Desktop;
+		}
 
-        /// <summary>
-        /// Shows the folder browser dialog box with the specified owner window.
-        /// </summary>
-        public DialogResult ShowDialog(IWin32Window owner)
-        {
-            IntPtr pidlRoot = IntPtr.Zero;
+		public DialogResult ShowDialog()
+		{
+			return ShowDialog(null);
+		}
+		
+		public DialogResult ShowDialog(IWin32Window owner)
+		{
+			IntPtr pidlRoot = IntPtr.Zero; //ルートノードのPIDLを保存する
 
-            // Get/find an owner HWND for this dialog.
-            IntPtr hWndOwner;
+			IntPtr hWndOwner = ( owner != null ) ? owner.Handle : Win32Api.GetActiveWindow();
 
-            if (owner != null)
-            {
-                hWndOwner = owner.Handle;
-            }
-            else
-            {
-                hWndOwner = Win32Api.GetActiveWindow();
-            }
+			// Get the IDL for the specific startLocation.
+			Win32Api.Shell32.SHGetSpecialFolderLocation( hWndOwner, (int)_startLocation, out pidlRoot );
 
-            // Get the IDL for the specific startLocation.
-            Win32Api.Shell32.SHGetSpecialFolderLocation(hWndOwner, (int)_startLocation, out pidlRoot);
+			if (pidlRoot == IntPtr.Zero)
+			{
+				return DialogResult.Cancel;
+			}
 
-            if (pidlRoot == IntPtr.Zero)
-            {
-                return DialogResult.Cancel;
-            }
+			int mergedOptions = (int)_publicOptions | (int)_privateOptions;
 
-            int mergedOptions = (int)_publicOptions | (int)_privateOptions;
+			if ((mergedOptions & (int)Win32Api.Shell32.BffStyles.NewDialogStyle) != 0)
+			{
+				if (System.Threading.ApartmentState.MTA == Application.OleRequired())
+					mergedOptions = mergedOptions & (~(int)Win32Api.Shell32.BffStyles.NewDialogStyle);
+			}
 
-            if ((mergedOptions & (int)Win32Api.Shell32.BffStyles.NewDialogStyle) != 0)
-            {
-                if (System.Threading.ApartmentState.MTA == Application.OleRequired())
-                    mergedOptions = mergedOptions & (~(int)Win32Api.Shell32.BffStyles.NewDialogStyle);
-            }
+			IntPtr pidlRet = IntPtr.Zero;
 
-            IntPtr pidlRet = IntPtr.Zero;
+			try
+			{
+				// Construct a BROWSEINFO.
+				Win32Api.Shell32.BROWSEINFO bi = new Win32Api.Shell32.BROWSEINFO();
+				IntPtr buffer = Marshal.AllocHGlobal(Win32Api.MAX_PATH);
 
-            try
-            {
-                // Construct a BROWSEINFO.
-                Win32Api.Shell32.BROWSEINFO bi = new Win32Api.Shell32.BROWSEINFO();
-                IntPtr buffer = Marshal.AllocHGlobal(Win32Api.MAX_PATH);
+				bi.pidlRoot       = pidlRoot;
+				bi.hwndOwner      = hWndOwner;
+				bi.pszDisplayName = buffer;
+				bi.lpszTitle      = DialogMessage;
+				bi.ulFlags        = mergedOptions;
+				// The rest of the fields are initialized to zero by the constructor.
+				// bi.lpfn = null;  bi.lParam = IntPtr.Zero;    bi.iImage = 0;
 
-                bi.pidlRoot       = pidlRoot;
-                bi.hwndOwner      = hWndOwner;
-                bi.pszDisplayName = buffer;
-                bi.lpszTitle      = _descriptionText;
-                bi.ulFlags        = mergedOptions;
-                // The rest of the fields are initialized to zero by the constructor.
-                // bi.lpfn = null;  bi.lParam = IntPtr.Zero;    bi.iImage = 0;
+				// Show the dialog.
+				pidlRet = Win32Api.Shell32.SHBrowseForFolder(ref bi);
 
-                // Show the dialog.
-                pidlRet = Win32Api.Shell32.SHBrowseForFolder(ref bi);
+				// Free the buffer you've allocated on the global heap.
+				Marshal.FreeHGlobal(buffer);
 
-                // Free the buffer you've allocated on the global heap.
-                Marshal.FreeHGlobal(buffer);
+				if (pidlRet == IntPtr.Zero)
+				{
+					// User clicked Cancel.
+					return DialogResult.Cancel;
+				}
 
-                if (pidlRet == IntPtr.Zero)
-                {
-                    // User clicked Cancel.
-                    return DialogResult.Cancel;
-                }
+				// Then retrieve the path from the IDList.
+				StringBuilder sb = new StringBuilder( Win32Api.MAX_PATH );
+				if (0 == Win32Api.Shell32.SHGetPathFromIDList(pidlRet, sb))
+				{
+					return DialogResult.Cancel;
+				}
 
-                // Then retrieve the path from the IDList.
-                StringBuilder sb = new StringBuilder( Win32Api.MAX_PATH );
-                if (0 == Win32Api.Shell32.SHGetPathFromIDList(pidlRet, sb))
-                {
-                    return DialogResult.Cancel;
-                }
-
-                // Convert to a string.
-                directoryPath = sb.ToString();
-            }
-            finally
-            {
+				// Convert to a string.
+				_directoryPath = sb.ToString();
+			}
+			finally
+			{
 				Win32Api.IMalloc malloc = Win32Api.Shell32.GetSHMalloc( );
-                malloc.Free(pidlRoot);
+				malloc.Free(pidlRoot);
 
-                if (pidlRet != IntPtr.Zero)
-                {
-                    malloc.Free(pidlRet);
-                }
-            }
+				if (pidlRet != IntPtr.Zero)
+				{
+					malloc.Free(pidlRet);
+				}
+			}
 
-            return DialogResult.OK;
-        }
+			return DialogResult.OK;
+		}
 	
  
-        public bool ShowNetworkFolders;
-        public bool OnlySubfolders;
-        public bool ShowTextBox;
-        public bool ValidateUserInput;
-        public bool SelectComputer;
-        public bool SelectPrinter;
-        public bool SelectFiles;
+		public bool OnlyFilesystem
+		{
+			get
+			{
+				return ( _publicOptions & (int)Win32Api.Shell32.BffStyles.RestrictToFilesystem ) != 0;
+			}
+			set
+			{
+				SetOptionField( (int)Win32Api.Shell32.BffStyles.RestrictToFilesystem, value );
+			}
+		}
+		public bool ShowNetworkFolders
+		{
+			get
+			{
+				return ( _publicOptions & (int)Win32Api.Shell32.BffStyles.RestrictToDomain ) != 0;
+			}
+			set
+			{
+				SetOptionField( (int)Win32Api.Shell32.BffStyles.RestrictToDomain, value );
+			}
+		}
+		public bool OnlySubfolders
+		{
+			get
+			{
+				return ( _publicOptions & (int)Win32Api.Shell32.BffStyles.RestrictToSubfolders ) != 0;
+			}
+			set
+			{
+				SetOptionField( (int)Win32Api.Shell32.BffStyles.RestrictToSubfolders, value );
+			}
+		}
+		public bool ShowTextBox
+		{
+			get
+			{
+				return ( _publicOptions & (int)Win32Api.Shell32.BffStyles.ShowTextBox ) != 0;
+			}
+			set
+			{
+				SetOptionField( (int)Win32Api.Shell32.BffStyles.ShowTextBox, value );
+			}
+		}
+		public bool ValidateUserInput
+		{
+			get
+			{
+				return ( _publicOptions & (int)Win32Api.Shell32.BffStyles.ValidateSelection ) != 0;
+			}
+			set
+			{
+				SetOptionField( (int)Win32Api.Shell32.BffStyles.ValidateSelection, value );
+			}
+		}
+		public bool SelectComputer
+		{
+			get
+			{
+				return ( _publicOptions & (int)Win32Api.Shell32.BffStyles.BrowseForComputer ) != 0;
+			}
+			set
+			{
+				SetOptionField( (int)Win32Api.Shell32.BffStyles.BrowseForComputer, value );
+			}
+		}
+		public bool SelectPrinter
+		{
+			get
+			{
+				return ( _publicOptions & (int)Win32Api.Shell32.BffStyles.BrowseForPrinter ) != 0;
+			}
+			set
+			{
+				SetOptionField( (int)Win32Api.Shell32.BffStyles.BrowseForPrinter, value );
+			}
+		}
+		public bool SelectFiles
+		{
+			get
+			{
+				return ( _publicOptions & (int)Win32Api.Shell32.BffStyles.BrowseForEverything ) != 0;
+			}
+			set
+			{
+				SetOptionField( (int)Win32Api.Shell32.BffStyles.BrowseForEverything, value );
+			}
+		}
 
-        public bool Description;
-
-        /// <summary>
-        /// Helper function used to set and reset bits in the publicOptions bitfield.
-        /// </summary>
-        private void SetOptionField(int mask, bool turnOn)
-        {
-            if (turnOn)
-                _publicOptions |= mask;
-            else
-                _publicOptions &= ~mask;
-        }
-
-        /// <summary>
-        /// Only return file system directories. If the user selects folders
-        /// that are not part of the file system, the OK button is unavailable.
-        /// </summary>
-        [Category("Navigation")]
-        [Description("Only return file system directories. If the user selects folders " +
-                       "that are not part of the file system, the OK button is unavailable.")]
-        [DefaultValue(true)]
-        public bool OnlyFilesystem
-        {
-            get
-            {
-                return (_publicOptions & (int)Win32Api.Shell32.BffStyles.RestrictToFilesystem) != 0;
-            }
-            set
-            {
-                SetOptionField((int)Win32Api.Shell32.BffStyles.RestrictToFilesystem, value);
-            }
-        }
-
-        /// <summary>
-        /// Location of the root folder from which to start browsing. Only the specified
-        /// folder and any folders beneath it in the namespace hierarchy  appear
-        /// in the dialog box.
-        /// </summary>
-        [Category("Navigation")]
-        [Description("Location of the root folder from which to start browsing. Only the specified " +
-                       "folder and any folders beneath it in the namespace hierarchy appear " +
-                       "in the dialog box.")]
-        [DefaultValue(typeof(FolderID), "0")]
-        public FolderID StartLocation
-        {
-            get
-            {
-                return _startLocation;
-            }
-            set
-            {
-                new UIPermission(UIPermissionWindow.AllWindows).Demand();
-                _startLocation = value;
-            }
-        }
-
-        /// <summary>
-        /// Full path to the folder selected by the user.
-        /// </summary>
-        [Category("Navigation")]
-        [Description("Full path to the folder slected by the user.")]
-        public string DirectoryPath
-        {
-            get
-            {
-                return directoryPath;
-            }
-        } 
-      
-    }
+	}
 
 }
--- TextureChanger/util/WinApi.cs
+++ TextureChanger/util/WinApi.cs
@@ -34,10 +34,10 @@ namespace TextureChanger
 		public const int SWP_NOZORDER = 0x0004;
 		public const int SWP_NOACTIVATE = 0x0010;
 
-        public const int MAX_PATH = 260;
+		public const int MAX_PATH = 260;
 
 
-        public struct RECT
+		public struct RECT
 		{
 			public RECT( int left, int top, int right, int bottom )
 			{
@@ -81,68 +81,68 @@ namespace TextureChanger
 
 
 
-        // C# representation of the IMalloc interface.
-        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown),
-           Guid("00000002-0000-0000-C000-000000000046")]
-        public interface IMalloc
-        {
-            [PreserveSig]
-            IntPtr Alloc([In] int cb);
-            [PreserveSig]
-            IntPtr Realloc([In] IntPtr pv, [In] int cb);
-            [PreserveSig]
-            void Free([In] IntPtr pv);
-            [PreserveSig]
-            int GetSize([In] IntPtr pv);
-            [PreserveSig]
-            int DidAlloc(IntPtr pv);
-            [PreserveSig]
-            void HeapMinimize();
-        }
+		// C# representation of the IMalloc interface.
+		[InterfaceType(ComInterfaceType.InterfaceIsIUnknown),
+		   Guid("00000002-0000-0000-C000-000000000046")]
+		public interface IMalloc
+		{
+			[PreserveSig]
+			IntPtr Alloc([In] int cb);
+			[PreserveSig]
+			IntPtr Realloc([In] IntPtr pv, [In] int cb);
+			[PreserveSig]
+			void Free([In] IntPtr pv);
+			[PreserveSig]
+			int GetSize([In] IntPtr pv);
+			[PreserveSig]
+			int DidAlloc(IntPtr pv);
+			[PreserveSig]
+			void HeapMinimize();
+		}
 
-        [DllImport("User32.DLL")]
-        public static extern IntPtr GetActiveWindow();
+		[DllImport("User32.DLL")]
+		public static extern IntPtr GetActiveWindow();
 
 
 		#region Shell32
 
 		public class Shell32
-        {
-            // Styles used in the BROWSEINFO.ulFlags field.
-            [Flags]
-            public enum BffStyles
-            {
-                RestrictToFilesystem = 0x0001, // BIF_RETURNONLYFSDIRS
-                RestrictToDomain = 0x0002, // BIF_DONTGOBELOWDOMAIN
-                RestrictToSubfolders = 0x0008, // BIF_RETURNFSANCESTORS
-                ShowTextBox = 0x0010, // BIF_EDITBOX
-                ValidateSelection = 0x0020, // BIF_VALIDATE
-                NewDialogStyle = 0x0040, // BIF_NEWDIALOGSTYLE
-                BrowseForComputer = 0x1000, // BIF_BROWSEFORCOMPUTER
-                BrowseForPrinter = 0x2000, // BIF_BROWSEFORPRINTER
-                BrowseForEverything = 0x4000, // BIF_BROWSEINCLUDEFILES
-            }
+		{
+			// Styles used in the BROWSEINFO.ulFlags field.
+			[Flags]
+			public enum BffStyles
+			{
+				RestrictToFilesystem = 0x0001, // BIF_RETURNONLYFSDIRS
+				RestrictToDomain     = 0x0002, // BIF_DONTGOBELOWDOMAIN
+				RestrictToSubfolders = 0x0008, // BIF_RETURNFSANCESTORS
+				ShowTextBox          = 0x0010, // BIF_EDITBOX
+				ValidateSelection    = 0x0020, // BIF_VALIDATE
+				NewDialogStyle       = 0x0040, // BIF_NEWDIALOGSTYLE
+				BrowseForComputer    = 0x1000, // BIF_BROWSEFORCOMPUTER
+				BrowseForPrinter     = 0x2000, // BIF_BROWSEFORPRINTER
+				BrowseForEverything  = 0x4000, // BIF_BROWSEINCLUDEFILES
+			}
 
-            // Delegate type used in BROWSEINFO.lpfn field.
-            public delegate int BFFCALLBACK(IntPtr hwnd, uint uMsg, IntPtr lParam, IntPtr lpData);
+			// Delegate type used in BROWSEINFO.lpfn field.
+			public delegate int BFFCALLBACK(IntPtr hwnd, uint uMsg, IntPtr lParam, IntPtr lpData);
 
-            [StructLayout(LayoutKind.Sequential, Pack = 8)]
-            public struct BROWSEINFO
-            {
-                public IntPtr hwndOwner;
-                public IntPtr pidlRoot;
-                public IntPtr pszDisplayName;
-                [MarshalAs(UnmanagedType.LPTStr)]
-                public string lpszTitle;
-                public int ulFlags;
-                [MarshalAs(UnmanagedType.FunctionPtr)]
-                public BFFCALLBACK lpfn;
-                public IntPtr lParam;
-                public int iImage;
-            }
+			[StructLayout(LayoutKind.Sequential, Pack = 8)]
+			public struct BROWSEINFO
+			{
+				public IntPtr hwndOwner;
+				public IntPtr pidlRoot;
+				public IntPtr pszDisplayName;
+				[MarshalAs(UnmanagedType.LPTStr)]
+				public string lpszTitle;
+				public int ulFlags;
+				[MarshalAs(UnmanagedType.FunctionPtr)]
+				public BFFCALLBACK lpfn;
+				public IntPtr lParam;
+				public int iImage;
+			}
 
-            [DllImport("Shell32.DLL")]
-            public static extern int SHGetMalloc(out IMalloc ppMalloc);
+			[DllImport("Shell32.DLL")]
+			public static extern int SHGetMalloc(out IMalloc ppMalloc);
 
 			public static Win32Api.IMalloc GetSHMalloc( )
 			{
@@ -152,20 +152,20 @@ namespace TextureChanger
 			}
 
 			[DllImport( "Shell32.DLL" )]
-            public static extern int SHGetSpecialFolderLocation(
-                        IntPtr hwndOwner, int nFolder, out IntPtr ppidl);
+			public static extern int SHGetSpecialFolderLocation(
+						IntPtr hwndOwner, int nFolder, out IntPtr ppidl);
 
-            [DllImport("Shell32.DLL")]
-            public static extern int SHGetPathFromIDList(
-                        IntPtr pidl, StringBuilder Path);
+			[DllImport("Shell32.DLL")]
+			public static extern int SHGetPathFromIDList(
+						IntPtr pidl, StringBuilder Path);
 
-            [DllImport("Shell32.DLL", CharSet = CharSet.Auto)]
-            public static extern IntPtr SHBrowseForFolder(ref BROWSEINFO bi);
+			[DllImport("Shell32.DLL", CharSet = CharSet.Auto)]
+			public static extern IntPtr SHBrowseForFolder(ref BROWSEINFO bi);
 			
 			
 		}
-        #endregion
+		#endregion
 
 
-    }
+	}
 }
